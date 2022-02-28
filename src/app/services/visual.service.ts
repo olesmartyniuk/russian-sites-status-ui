f5293f3e4431c1330f8b2fbf59ab9763dcba9f6e
Pavlo <sofronov.pavlo@outlook.com>
2022-02-28 16:16:08 -0500

    Styled details page

---
+++ src/app/services/visual.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class VisualService {
+
+    public async Init(): Promise<void> {
+        // do something on App start
+    }
+
+    public getColorStyleForStatusBage(status: number) {
+        if (status == 0)
+            return "downStyle";
+
+        if (status == 100)
+            return "upStyle";
+
+        return "upDownStyle";
+    }
+
+    public getColorStyleForStatusChip(status: string) {
+        if (status == 'up')
+            return "upStyle";
+        return "downStyle";
+    }
+}

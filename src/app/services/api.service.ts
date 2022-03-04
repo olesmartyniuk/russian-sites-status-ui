60a0e11cd8fca96c3ab0fba9de9cceb2ceaca8ca
Vasyl Kosovan <vasyl.kosovan@innroad.com>
2022-03-04 14:15:00 +0200

    fix/use https instead of http

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Site, SiteDetails } from '../models/site';
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl: string = 'http://api.mordor-sites-status.info/api';
+  private baseUrl: string = 'https://api.mordor-sites-status.info/api';
 
   constructor(private http: HttpClient) { }
 

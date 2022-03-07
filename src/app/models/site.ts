45c0f20e86195762f5d8006602e4baed35da0bd1
Oleksandr Martyniuk <alexander.martyniuk@gmail.com>
2022-03-06 20:25:02 -0500

    Removed redundant properties from VM

--- src/app/models/site.ts
+++ src/app/models/site.ts
@@ -1,23 +1,18 @@
 export class Site {
-    id: string;
+    id: number;
     name: string;
-    testType: string;
     status: string;
     uptime: number;
 }
 
 export class SiteDetails extends Site {
-    doNotFind: boolean;
     servers: Server[];
-    processing: boolean;
-    timeout: number;
     lastTestedAt: string;
 }
 
 export class Server {
-    description: string;
     region: string;
     regionCode: string;
     status: string;
-    ipv4: string;
+    statusCode: number;
 }
